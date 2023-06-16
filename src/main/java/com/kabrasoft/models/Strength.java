604d43c17bc41d87a824ab48fbbbb413d5237a86
ombasaMukhwami <mukhwami@gmail.com>
2023-06-16 22:05:06 +0300

    remove inheritance from Strength

--- src/main/java/com/kabrasoft/models/Strength.java
+++ src/main/java/com/kabrasoft/models/Strength.java
@@ -1,7 +1,42 @@
 package com.kabrasoft.models;
 
-public class Strength extends Power {
+public class Strength {
+    private int id;
+    private String name;
+    private int score;
+
     public Strength() {
+
+    }
+
+    public Strength(int id, String name, int score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
 }
