efb3ec73f90e177c540f1e8ab93161660647edc2
ombasaMukhwami <mukhwami@gmail.com>
2023-06-16 22:18:35 +0300

    move weakness list to helper class

--- src/main/java/com/kabrasoft/Main.java
+++ src/main/java/com/kabrasoft/Main.java
@@ -9,12 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Weakness> weaknessList = new ArrayList<>();
-        weaknessList.add(new Weakness(1,"level 0", 0));
-        weaknessList.add(new Weakness(2,"level 1", 10));
-        weaknessList.add(new Weakness(3,"level 2", 20));
-        weaknessList.add(new Weakness(4,"level 3", 30));
-        weaknessList.add(new Weakness(5,"level 4", 40));
+
 
         List<Strength> strengthList = new ArrayList<>();
         strengthList.add(new Strength(1,"level 0", 50));
--- src/main/java/com/kabrasoft/utils/Helper.java
+++ src/main/java/com/kabrasoft/utils/Helper.java
@@ -1,7 +1,22 @@
 package com.kabrasoft.utils;
 
+import com.kabrasoft.models.Weakness;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Helper {
     private Helper() {
 
+    }
+
+    public  static List<Weakness> getWeakness(){
+        List<Weakness> weaknessList = new ArrayList<>();
+        weaknessList.add(new Weakness(1,"level 0", 0));
+        weaknessList.add(new Weakness(2,"level 1", 10));
+        weaknessList.add(new Weakness(3,"level 2", 20));
+        weaknessList.add(new Weakness(4,"level 3", 30));
+        weaknessList.add(new Weakness(5,"level 4", 40));
+        return weaknessList;
     }
 }
